4911400fbe3f401d54b9923a50fedbaf10a3a74a
Juhani <juhani@fmail.co.uk>
2023-02-01 13:18:48 +0200

    Rework user authentication to use JWT (#10)
    Reworked user authentication to use JWT instead of Spring builtin user authentication i.e. formlogin.

---
+++ src/main/java/fi/asteriski/eventsignup/user/AuthCredentialsRequest.java
@@ -0,0 +1,21 @@
+/*
+Copyright Juhani Vähä-Mäkilä (juhani@fmail.co.uk) 2022.
+Licenced under EUROPEAN UNION PUBLIC LICENCE v. 1.2.
+ */
+package fi.asteriski.eventsignup.user;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class AuthCredentialsRequest {
+
+    @NonNull
+    private String username;
+    @NonNull
+    private String password;
+}
