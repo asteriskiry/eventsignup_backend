761b0a55ca21283379fea01cdc82a343857ebbcd
aegan123 <juhani@fmail.co.uk>
2023-02-14 12:24:51 +0200

    Move Quota type to its own file for better readability

---
+++ src/main/java/fi/asteriski/eventsignup/domain/Quota.java
@@ -0,0 +1,8 @@
+/*
+Copyright Juhani Vähä-Mäkilä (juhani@fmail.co.uk) 2023.
+Licenced under EUROPEAN UNION PUBLIC LICENCE v. 1.2.
+ */
+package fi.asteriski.eventsignup.domain;
+
+record Quota(String group, String quota) {
+}
