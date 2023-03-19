72017468c653ac7b71d15b36050ab2f581ec7c3d
Juhani Vähä-Mäkilä <juhani@fmail.co.uk>
2023-03-19 09:41:19 +0200

    Define remaining api endpoints as constants

--- src/main/java/fi/asteriski/eventsignup/Constants.java
+++ src/main/java/fi/asteriski/eventsignup/Constants.java
@@ -6,4 +6,7 @@ package fi.asteriski.eventsignup;
 
 public final class Constants {
     public static final String API_PATH_ARCHIVE = "/api/archive";
+    public static final String API_PATH_ADMIN = "/api/admin";
+    public static final String API_PATH_EVENT = "/api/event";
+    public static final String API_PATH_SIGNUP = "/api/signup";
 }
