90b6c4734056260d8a0b078442e307260d1528d0
Juhani Vähä-Mäkilä <juhani@fmail.co.uk>
2024-01-28 11:16:02 +0200

    Add missing interfaces

---
+++ src/main/java/fi/asteriski/eventsignup/dao/archiving/ArchivedEventDaoImpl.java
@@ -0,0 +1,62 @@
+/*
+Copyright Juhani Vähä-Mäkilä (juhani@fmail.co.uk) 2024.
+Licenced under EUROPEAN UNION PUBLIC LICENCE v. 1.2.
+ */
+package fi.asteriski.eventsignup.dao.archiving;
+
+import fi.asteriski.eventsignup.domain.archiving.ArchivedEventDto;
+import fi.asteriski.eventsignup.domain.archiving.ArchivedEventEntity;
+import fi.asteriski.eventsignup.repo.archiving.ArchivedEventRepository;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotNull;
+import java.time.Instant;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ArchivedEventDaoImpl implements ArchivedEventDao {
+    @NonNull
+    private ArchivedEventRepository archivedEventRepository;
+
+    @Override
+    public ArchivedEventDto save(@NotNull final ArchivedEventDto toSave) {
+        return archivedEventRepository.save(toSave.toEntity()).toDto();
+    }
+
+    @Override
+    public List<ArchivedEventDto> saveAll(@NotNull final List<ArchivedEventDto> toSave) {
+        var items = toSave.stream()
+            .map(ArchivedEventDto::toEntity)
+            .toList();
+        return archivedEventRepository.saveAll(items).stream()
+            .map(ArchivedEventEntity::toDto)
+            .toList();
+    }
+
+    @Override
+    public List<ArchivedEventDto> findAll() {
+        return archivedEventRepository.findAll().stream()
+            .map(ArchivedEventEntity::toDto)
+            .toList();
+    }
+
+    @Override
+    public List<ArchivedEventDto> findAllByOriginalOwner(@NotNull final String userId) {
+        return archivedEventRepository.findAllByOriginalOwner(userId).stream()
+            .map(ArchivedEventEntity::toDto)
+            .toList();
+    }
+
+    @Override
+    public void deleteAllByDateArchivedIsBefore(@NotNull final Instant dateLimit) {
+        archivedEventRepository.deleteAllByDateArchivedIsBefore(dateLimit);
+    }
+
+    @Override
+    public void deleteById(@NotNull final String archivedEventId) {
+        archivedEventRepository.deleteById(archivedEventId);
+    }
+}
