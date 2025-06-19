a31f53cde7fd438b031de915541599d7967a658d
Juhani Vähä-Mäkilä <juhani@fmail.co.uk>
2025-06-19 15:42:36 +0300

    Fix cyclic dependencies

--- src/main/java/fi/asteriski/eventsignup/dao/FormDao.java
+++ src/main/java/fi/asteriski/eventsignup/dao/FormDao.java
@@ -4,23 +4,24 @@ Licenced under EUROPEAN UNION PUBLIC LICENCE v. 1.2.
  */
 package fi.asteriski.eventsignup.dao;
 
-import static fi.asteriski.eventsignup.utils.Constants.FORM_NOT_FOUND_EXCEPTION_SUPPLIER;
-
 import fi.asteriski.eventsignup.dao.entity.EventEntity;
 import fi.asteriski.eventsignup.dao.entity.FormEntity;
 import fi.asteriski.eventsignup.dao.repository.FormRepository;
 import fi.asteriski.eventsignup.dto.FormDto;
-import fi.asteriski.eventsignup.service.event.EventService;
 import jakarta.validation.constraints.NotNull;
-import java.util.List;
-import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+import static fi.asteriski.eventsignup.utils.Constants.EVENT_NOT_FOUND_EXCEPTION_SUPPLIER;
+import static fi.asteriski.eventsignup.utils.Constants.FORM_NOT_FOUND_EXCEPTION_SUPPLIER;
 
 @Component
 @AllArgsConstructor
 public class FormDao {
-    private final EventService eventService;
+    private final EventDao eventService;
     private final FormRepository formRepository;
 
     public List<FormDto> fetchForms(@NotNull List<UUID> formIds) {
@@ -47,7 +48,8 @@ public class FormDao {
     private EventEntity getEventEntity(@NotNull final FormDto formDto, @NotNull final FormEntity oldForm) {
         EventEntity event = null;
         if (!oldForm.getEvent().getId().equals(formDto.eventId())) {
-            event = eventService.fetchEventForSignupById(formDto.eventId());
+            event = eventService.fetchEventEntity(formDto.eventId())
+                .orElseThrow(EVENT_NOT_FOUND_EXCEPTION_SUPPLIER);
         }
         return event;
     }
